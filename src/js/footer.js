ce6ca340f6018803a7342b9d36257bf8d1913de0
Andrii888 <aotk888@gmail.com>
2025-12-14 21:59:54 +0200

    add js for links

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,3 +1,4 @@
+// плавний скрол
 document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelectorAll('.footer-nav-link');
   
@@ -14,6 +15,31 @@ document.addEventListener('DOMContentLoaded', function () {
           });
         }
       }
+    });
+  });
+});
+
+// стан active на лінках та disabled
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.footer-nav-link');
+  if (!links.length) return;
+
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+     
+      if (link.classList.contains('is-disabled')) return;
+
+           links.forEach(l => l.classList.remove('is-active'));
+
+           link.classList.add('is-active');
+
+            const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+
+         history.pushState(null, '', targetId);
     });
   });
 });
